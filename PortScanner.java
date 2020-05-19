b4822fc0d04a318b673aaf146cba91a1f5b8aeb9
Roman Yaschenko <56332169+rq-dev@users.noreply.github.com>
2020-05-19 13:53:26 +0500

    Update PortScanner.java

--- PortScanner.java
+++ PortScanner.java
@@ -5,7 +5,7 @@ public class PortScanner {
     public static void main(String[] args) {
         try {
             if ((args.length == 0) || (args[0]).equals("help") || (args[0]).equals("Help")) {
-                System.out.println("PortScanner.jar by Roman Yaschenko MO-202\nThe program checks if TCP ports are open or closed");
+                System.out.println("PortScanner.jar by Roman Yaschenko MO-202\nThe program checks whether TCP ports are open or closed");
                 System.out.println("Run: \njava -jar PortScanner.jar [Port range start]" +
                         "[Port range end][host (If not specified then Localhost)]\n" +
                         "Example: \njava -jar PortScanner.jar 1 " +
